104cbc5ebfb2e32b60ad4889a17a1ae9554416b4
MasterEvarior <masterevarior@protonmail.com>
2020-05-24 22:52:41 +0200

    Initial commit

---
+++ src/main/java/com/giannin/freesteamgamesapi/FreeSteamGamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.giannin.freesteamgamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeSteamGamesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeSteamGamesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giannin/freesteamgamesapi/FreeSteamGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giannin.freesteamgamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeSteamGamesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
