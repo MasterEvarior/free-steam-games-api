65cc10d09fb006e875f59df61098806fad9fd3fc
MasterEvarior <masterevarior@protonmail.com>
2020-05-26 21:29:34 +0200

    Added the basics of the other services

---
+++ src/main/java/com/giannin/freesteamgamesapi/service/CategoryService.java
@@ -0,0 +1,26 @@
+package com.giannin.freesteamgamesapi.service;
+
+import com.giannin.freesteamgamesapi.model.Category;
+import com.giannin.freesteamgamesapi.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository repository;
+
+    public Category save(Category input){
+        return this.repository.save(input);
+    }
+
+    public Optional<Category> getById(Integer id){
+        return this.repository.findById(id);
+    }
+
+    public void delete(Integer id){
+        this.repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/giannin/freesteamgamesapi/service/GenreService.java
@@ -0,0 +1,26 @@
+package com.giannin.freesteamgamesapi.service;
+
+import com.giannin.freesteamgamesapi.model.Genre;
+import com.giannin.freesteamgamesapi.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class GenreService {
+    @Autowired
+    private GenreRepository repository;
+
+    public Genre save(Genre input){
+        return repository.save(input);
+    }
+
+    public Optional<Genre> getById(Integer id){
+        return repository.findById(id);
+    }
+
+    public void delete(Integer id){
+        repository.deleteById(id);
+    }
+}
