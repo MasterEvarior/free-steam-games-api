fc0b2910518ee562a3ace7f81c549382d51aaa99
MasterEvarior <masterevarior@protonmail.com>
2020-05-26 12:51:18 +0200

    Added models as entities to JPA

--- src/main/java/com/giannin/freesteamgamesapi/model/Game.java
+++ src/main/java/com/giannin/freesteamgamesapi/model/Game.java
@@ -1,8 +1,13 @@
 package com.giannin.freesteamgamesapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.List;
 
+@Entity
 public class Game {
+    @Id
     private Integer steamId;
     private String name;
     private Integer requiredAge;
@@ -10,6 +15,8 @@ public class Game {
     private String thumbnail;
     private String website;
     private String releaseDate;
+    @OneToMany
     private List<Genre> genres;
+    @OneToMany
     private List<Category> categories;
 }
--- src/main/java/com/giannin/freesteamgamesapi/model/Genre.java
+++ src/main/java/com/giannin/freesteamgamesapi/model/Genre.java
@@ -1,6 +1,11 @@
 package com.giannin.freesteamgamesapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Genre {
+    @Id
     private Integer id;
     private String description;
 }
